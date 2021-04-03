b5f80ad0099e6ee560a9f48a6bc583ebd90e9987
John George <Sci.JohnGeorge.2017@alexu.edu.eg>
2021-04-03 17:54:16 +0200

    Optimize application resources

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,7 @@
 import "admin-lte/dist/js/adminlte.min"
-import "admin-lte/dist/css/adminlte.min.css"
-import "admin-lte/plugins/fontawesome-free/css/all.min.css"
 import "admin-lte/plugins/jquery/jquery.min"
 import "admin-lte/plugins/bootstrap/js/bootstrap.bundle"
 
-import "datatables.net-bs4/css/dataTables.bootstrap4.css";
-import "datatables.net-buttons-bs4/css/buttons.bootstrap4.css";
 import "datatables.net/js/jquery.dataTables.min"
 
 require('./bootstrap');
